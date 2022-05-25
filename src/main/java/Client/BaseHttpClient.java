73eb16ae4bb8bac957434a807ae00ea1b735861b
BeknurN <beknur.narmukhan@kaspi.kz>
2022-05-25 16:51:02 +0600

    1

---
+++ src/main/java/Client/BaseHttpClient.java
@@ -0,0 +1,14 @@
+package Client;
+
+public class BaseHttpClient {
+
+    /**
+     * Тип  содержимого запроса.
+     */
+    public static final String HEADER_CONTENT_TYPE = "application/json";
+
+    /**
+     * Хост API.
+     */
+    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+}
